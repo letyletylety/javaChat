b37c9ef923f133c9517e19cc6f64a8bfbd6e943e
jhoowy <jhoojh1@naver.com>
2017-06-01 21:51:36 +0900

    Add files via upload

---
+++ LoginHandler.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.HashMap;
+
+public class LoginHandler {
+	private FileHandler fs = null;
+	private HashMap<String, String> user = new HashMap<String, String>();
+	private HashMap<String, Integer> logged = new HashMap<String, Integer>(); 
+	
+	public LoginHandler (String filename) throws IOException {
+		File login_file = new File(filename);
+		try {
+			if (login_file.exists() != true){
+       	 		login_file.createNewFile();
+        	}
+		}
+		catch (IOException ioe){
+			System.out.println("Login File Error :" + ioe.getMessage());
+		}
+		fs = new FileHandler(filename);
+	}
+	
+	public void init() throws IOException{
+		String temp;
+		String s[];
+		while ((temp = fs.ReadLine()) != null){
+			s = temp.split(" ");
+			user.put(s[0], s[1]);
+		}
+	}
+	
+	public boolean create(String ID, String PW) throws IOException{
+		String temp;
+		if (user.containsKey(ID)){
+			return false;
+		}
+		user.put(ID, PW);
+		logged.put(ID, 1);
+		temp = ID + " " + PW;
+		fs.Write(temp);
+		return true;
+	}
+	
+	public int signIn(String ID, String PW){
+		if (user.containsKey(ID) != true)
+			return 0;
+		if (user.get(ID) != PW)
+			return 1;
+		if (logged.containsKey(ID))
+			return 2;
+		
+		logged.put(ID, 1);
+		return 3;
+	}
+	
+	public void signOut(String ID){
+		if (logged.containsKey(ID) != true){
+			System.out.println("Error : User is not signed in");
+			return;
+		}
+		logged.remove(ID);
+	}
+	
+	
+}
